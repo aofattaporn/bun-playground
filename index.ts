8b8779924e3eab32222b5ef63af60c28ca77963c
aofattaporn <aofattapon321@gmail.com>
2023-10-08 15:38:20 +0700

    add http server code

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  port: 3000,
+  fetch(req) {
+    const url = new URL(req.url);
+    if (url.pathname === "/") return new Response("Home page!");
+    if (url.pathname === "/blog") return new Response("Blog!");
+    return new Response("404!");
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
