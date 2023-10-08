20a6fc148f66682b7c03b306f2683873cdd61f8e
“aofattaporn” <aofattapon321@gmail.com>
2023-10-09 00:12:46 +0700

    test

--- controllers/blog.ts
+++ controllers/blog.ts
@@ -2,7 +2,7 @@ import Bao from "baojs";
 import { mock_harry_potter } from "../mocks/blogMock";
 
 export const blog_controller = (app: Bao) => {
-  // get blogss
+  // get blogssss
   app.get("/", (ctx) => {
     return ctx.sendPrettyJson(mock_harry_potter);
   });
