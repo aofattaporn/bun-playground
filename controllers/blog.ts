363706c71a64c4654703a20cc86f0e655f11f35c
aofattaporn <aofattapon321@gmail.com>
2023-10-22 17:31:56 +0700

    connect moogo db

--- controllers/blog.ts
+++ controllers/blog.ts
@@ -1,24 +1,43 @@
 import Bao from "baojs";
+import { MongoClient } from "mongodb";
 import { blogs } from "../mocks/blogMock";
 import { BlogType } from "../models/Blog";
+const pino = require("pino");
+const logger = pino({
+  level: "info",
+});
 
-export const blog = (app: Bao) => {
-  // get all blog
-  app.get("/api/blogs", (ctx) => {
-    return ctx.sendPrettyJson(blogs);
+export const blog = async (app: Bao, client: MongoClient) => {
+  // declare database collection
+  const db = client.db("test");
+  const collectionx = db.collection("book");
+
+  // get all blogs
+  app.get("/api/blogs", async (ctx) => {
+    try {
+      logger.info("/api/blogs");
+      const posts = await collectionx.find({}).toArray();
+
+      return ctx.sendPrettyJson(posts);
+    } catch (error) {
+      console.error("Error fetching blogs:", error);
+      return ctx.sendText("error");
+    }
   });
 
-  // get blog id
+  // get blog by blog id
   app.get("/api/blogs/:blog_id", (ctx) => {
     const blogId = ctx.params.blog_id;
     const blog = blogs.find((blog) => blog.id === parseInt(blogId));
-
     return ctx.sendPrettyJson(blog as BlogType);
   });
 
-  // create blog
+  // add new blog
+  app.post("/api/blogs", (ctx) => {
+    return ctx.sendText("Blog created successfully");
+  });
 
-  // update blog
+  // delete blog by id
 
-  // delete blog
+  // delete all blog
 };
--- index.ts
+++ index.ts
@@ -1,12 +1,31 @@
-// index.ts
 import Bao from "baojs";
+import { MongoClient, ServerApiVersion } from "mongodb";
 import { controllers } from "./controllers";
 
-// create instant to ceate a server
-const app = new Bao();
+async function startServer() {
+  const uri = process.env.MONGO_URL;
 
-// calling controller
-controllers.blog(app);
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
 
-const server = app.listen();
-console.log(`Listening on ${server.hostname}:${server.port}`);
+  try {
+    console.log("Connecting to MongoDB...");
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    const app = new Bao();
+    await controllers.blog(app, client);
+
+    const server = app.listen();
+    console.log(`Listening on ${server.hostname}:${server.port}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+startServer();
